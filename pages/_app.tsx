4c86aa292fef7196e52f50f89e33d21005bde75e
“martinmorales0106” <marmorji123456@gmail.com>
2023-08-11 14:03:40 -0500

    _app y _document para hacer cambios generales en la aplicacion

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+// _app.tsx es un componente especial que se utiliza para personalizar y envolver la aplicación con componentes o lógica global antes de que se rendericen las páginas individuales. Es un componente de nivel superior que se utiliza para realizar configuraciones generales, manejar estado global, aplicar estilos globales y realizar otras acciones comunes en toda la aplicación.
+
+import { AppProps } from 'next/app';
+import React from 'react';
+// import '../styles/globals.css'; // podemos utilizar estilos globales
+import Layout from '@/components/Layout/Layout';
+
+/**
+ * Componente raíz de la aplicación.
+ * Envuelve cada página con el componente Layout para proporcionar una estructura común.
+ * @param Component - Componente de la página actual.
+ * @param pageProps - Propiedades pasadas a la página.
+ */
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      {/* El componente Layout envuelve cada página */}
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from "../components/Navbar/Navbar"
 import CardsBooks from '@/components/CardsBooks/CardsBooks'
 
 
 function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Home</h1>
       <CardsBooks/>
     </div>
