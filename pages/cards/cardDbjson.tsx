35442f4875767aced9d4fd3ad40ce4941b1b9bfa
CesarGoMel <118846121+CesarGoMel@users.noreply.github.com>
2023-08-10 23:12:15 -0500

    Initial Book

---
+++ pages/cards/cardDbjson.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function cardDbjson (req, res) => {
+    try {
+
+        
+    } catch (error) {
+        
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default cardDbjson
