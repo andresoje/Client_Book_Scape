849f64695e9ee28faed35a4778e91ccfe0f0cc46
CesarGoMel <118846121+CesarGoMel@users.noreply.github.com>
2023-08-14 19:26:24 -0500

    cambios css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardsBooks from "@/components/CardsBooks/CardsBooks";
 import styles from "../styles/Home.module.css";
 import { useBookContext } from "@/context/BookContext";
+import autor from "../public/images/autor.png";
 
 function Home() {
   // Utiliza el hook useBookContext para obtener los datos y funciones del contexto
@@ -12,15 +13,19 @@ function Home() {
       {/* //columna izquierda */}
       <div className={styles.descriptionIzq}>
         <h3>Consulta por categorias</h3>
-        <p>Poseia</p>
-        <p>Poseia</p>
-        <p>Poseia</p>
-        <p>Poseia</p>
+        <p>Historia</p>
+        <p>Poesia</p>
+        <p>Ficción</p>
+        <p>Novelas</p>
       </div>
       {/* //columna derecha */}
       <div className={styles.descriptionDer}>
-        <h3 className={styles.h1}>Bienvenidos</h3>
-        <CardsBooks books={books} />
+        <div><h3 className={styles.h1}>Bienvenidos</h3>
+        <CardsBooks books={books} /></div>
+        <div><h3 className={styles.h1}>Libro Destacado</h3>       
+        </div>
+        <div  className={styles.autor}><img src={autor.src} alt="autor" /></div>
+    
       </div>
     </div>
   );
