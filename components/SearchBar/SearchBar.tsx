1e13abf4da83193253f256f3b8e1a00b4eecabf9
“martinmorales0106” <marmorji123456@gmail.com>
2023-08-19 09:02:49 -0500

    Listo el buscador por titulo, genero y autor

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,17 +1,41 @@
-import React from 'react'
-import styles from './SearchBar.module.css'
+import React, { useState } from "react";
+import styles from "./SearchBar.module.css";
+import { useRouter } from 'next/router';
 
-function SearchBar( ) {
-   
+function SearchBar() {
+  const [busqueda, guardarBusqueda] = useState("");
+  const router = useRouter();
+
+  const buscarProducto = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    if (busqueda.trim() === '') {
+      router.push({
+        pathname: '/',
+        query: { q: busqueda }
+      });
+    } else {
+      // redireccionar a /buscar
+      router.push({
+        pathname: '/buscar',
+        query: { q: busqueda }
+      });
+    }
+  };
+
   return (
-    <div> 
-    <div className={styles.inputbox} >
-      <input type="text" placeholder="¿Que quieres leer Hoy?" />
-      <button className={styles.button}>Buscar</button>
+    <div>
+      <form className={styles.inputbox} onSubmit={buscarProducto}>
+        <input
+          type="text"
+          placeholder="¿Qué quieres leer hoy?"
+          value={busqueda}
+          onChange={(e) => guardarBusqueda(e.target.value)}
+        />
+        <button className={styles.button} type="submit">Buscar</button>
+      </form>
     </div>
-    </div>
-   
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
