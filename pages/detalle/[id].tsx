ba07922f66a465c2e763d7a5214ad63aa5a13649
“martinmorales0106” <marmorji123456@gmail.com>
2023-08-21 18:51:13 -0500

    correciones en index de id

--- pages/detalle/[id].tsx
+++ pages/detalle/[id].tsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useBookContext } from "@/context/BookContext";
 import styles from "./detail.module.css";
-import Rating from "../../components/Rating/Rating";
 import Link from "next/link";
 import { IoIosCart } from "react-icons/io";
 import { useCartContext } from "@/context/CartContext";
 
 const DetallesBook = () => {
+  const [cantidad, setCantidad] = useState(1);
   const { books } = useBookContext();
 
   const {
@@ -17,62 +17,60 @@ const DetallesBook = () => {
   const detallebook = books.find((book) => book.id === Number(id));
 
   // carrito de compra
-  const { addToCart } = useCartContext();
+  const { agregarCarrito, cartItems } = useCartContext();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (cantidad < 1) {
+      alert("Cantidad no válida");
+      return;
+    }
+
+    if (detallebook) {
+      const cartItem = {
+        id: detallebook.id,
+        title: detallebook.title,
+        price: detallebook.price,
+        image: detallebook.image,
+        authors: detallebook.authors,
+        cantidad,
+      };
+
+      agregarCarrito(cartItem);
+    }
+  };
 
   return (
     <div>
       {detallebook ? (
         <div className={styles.container}>
-          <div className={styles.izquierda}>
-            <Link href={"/"}>Regresar</Link>
-            <div className={styles.imagen}>
-              <img src={detallebook.image} alt={detallebook.title} />
-
-              <Rating ratingCount={detallebook.rating_ave} />
-            </div>
-          </div>
-          <div className={styles.derecha}>
-            <div className={styles.titulo}>
-              {" "}
-              <h1>{detallebook.title}</h1>{" "}
-            </div>
-            <div className={styles.autor}>
-              {" "}
-              <h3>{detallebook.authors}</h3>{" "}
-            </div>
-            <div className={styles.descripcion}>
-              {" "}
-              <h4>Descripción:</h4> <p> {detallebook.description}</p>{" "}
-            </div>
-
-            <div className={styles.detalles}>
-              <div>
-                {" "}
-                <h2>Precio: ${detallebook.price}</h2>{" "}
-              </div>
-              <div>Genero: {detallebook.tags}</div>
-              <form className={styles.formulario}>
-                <label htmlFor="cantidad">Cantidad:</label>
-                <select id="cantidad">
-                  <option value="0">-- Seleccione --</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                </select>
-                <input type="submit" value="Agregar al Carrito" />
-              </form>
-              <Link href={`/CarritoDeCompra`}>
-                <button
-                  onClick={() => addToCart(detallebook)}
-                  className={styles.button}
-                >
-                  <IoIosCart />
-                  ir al carrito
-                </button>
-              </Link>
-            </div>
+          {/* ...contenido del detalle... */}
+          <div className={styles.detalles}>
+            {/* ...otros detalles... */}
+            <form className={styles.formulario} onSubmit={handleSubmit}>
+              <label htmlFor="cantidad">Cantidad:</label>
+              <select
+                id="cantidad"
+                onChange={(e) => setCantidad(+e.target.value)}
+              >
+                <option value="0">-- Seleccione --</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+              <button type="submit" className={styles.button}>
+                Agregar al Carrito
+              </button>
+            </form>
+            <Link href={`/CarritoDeCompra`}>
+              <button className={styles.button}>
+                <IoIosCart />
+                Ir al Carrito
+              </button>
+            </Link>
           </div>
         </div>
       ) : (
