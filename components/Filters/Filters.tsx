9dd10b0a8a769b83a8b7108ee2d291e5988a7b7a
“martinmorales0106” <marmorji123456@gmail.com>
2023-08-23 07:28:59 -0500

    Filtros agregados

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,94 +1,147 @@
-import React, { useState, useEffect } from 'react';
-import { useFilterContext } from '@/context/FilterContext';
-import styles from "./Filters.module.css"
-import CardBook from '../CardBook/CardBook';
+import React, { useState } from "react";
+import styles from "./Filters.module.css";
+import CardBook from "../CardBook/CardBook";
+import { useFilterContext } from "@/context/FilterContext";
+import axios from "axios";
+
+interface Author {
+  name: string;
+}
+
+interface Tags {
+  name: string;
+}
+
 interface Book {
-  id: number;
+  id_book: number;
   title: string;
-  authors: string;
+  Authors: Author[];
   price: number;
-  description: string;
-  rating: number;
+  Tags: Tags[];
   image: string;
-  flag: string;
-  currency: string;
-  ratingCount: number;
-  Tags: string[];
-  language: string;
-  stock: number;
+  rating_ave: number;
 }
 
 interface FilterState {
   price: number;
   language: string;
   category: string;
+  selectedTags: string[];
 }
 
 const Filtros: React.FC = () => {
- // const { books } = useFilterContext();
+  const { books, uniqueLanguages, uniqueTags } = useFilterContext();
+
   const [filters, setFilters] = useState<FilterState>({
     price: 0,
-    language: '',
-    category: '',
+    language: "",
+    category: "",
+    selectedTags: [],
   });
 
-  const [uniqueLanguages, setUniqueLanguages] = useState<string[]>([]);
-  const [uniqueTags, setUniqueTags] = useState<string[]>([]);
-  const [filteredbooks, setFilteredbooks] = useState<Book[]>([]);
-  return (
-    <div className= {styles.container} >
-     <div >
-   {/* Filtros por precio */}
-  <label>Precio</label><br />
-  0 <input  value={filters.price} type="range" id="filters.price" name="filters.price" min="0" max="50" 
-  onChange={(e) => setFilters({ ...filters, price: Number(e.target.value) })} />50
-</div> <br />
+  const [filteredBooks, setFilteredBooks] = useState<Book[]>(books);
 
-    
+  const applyFilters = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/books/filter", {
+        params: {
+          tags: filters.selectedTags.join(","),
+          language: filters.language,
+          price: filters.price,
+        },
+      });
+      setFilteredBooks(response.data);
+    } catch (error) {
+      console.error("Error applying filters:", error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* Filtros por precio */}
+      <label>Precio</label>
+      <br />
+      0{" "}
+      <input
+        value={filters.price}
+        type="range"
+        id="filters.price"
+        name="filters.price"
+        min="0"
+        max="50"
+        onChange={(e) =>
+          setFilters({ ...filters, price: Number(e.target.value) })
+        }
+      />
+      <span>{filters.price}</span>
+      <br />
 
       {/* Filtros por idioma */}
-      <select className= {styles.input}
-        value={filters.language} id='filters.language' name='filters.language'
+      <select
+        className={styles.input}
+        value={filters.language}
+        id="filters.language"
+        name="filters.language"
         onChange={(e) => setFilters({ ...filters, language: e.target.value })}
       >
         <option value="">Todos los idiomas</option>
-        {uniqueLanguages.map(language => (
-          <option key={language} value={language}>{language}</option>
+        {uniqueLanguages.map((language) => (
+          <option key={language} value={language}>
+            {language}
+          </option>
         ))}
       </select>
 
-      {/* Filtros por género */}
-      <select className= {styles.input}
-        value={filters.category} id='filters.category' name='filters.category'
-        onChange={(e) => setFilters({ ...filters, category: e.target.value })}
-      >
-        <option value="">Todos los géneros</option>
-        {uniqueTags.map(Tags => (
-          <option key={Tags} value={Tags} >{Tags}</option>
-        ))}
-      </select>
+        <br/>
+      {/* Filtros por tags */}
+      {uniqueTags.map((tag) => (
+        <label key={tag}>
+          <input
+            type="checkbox"
+            value={tag}
+            checked={filters.selectedTags.includes(tag)}
+            onChange={(e) => {
+              if (e.target.checked) {
+                setFilters({
+                  ...filters,
+                  selectedTags: [...filters.selectedTags, tag],
+                });
+              } else {
+                setFilters({
+                  ...filters,
+                  selectedTags: filters.selectedTags.filter(
+                    (selectedTag) => selectedTag !== tag
+                  ),
+                });
+              }
+            }}
+          />
+          {tag}
+        </label>
+      ))}
 
       {/* Botón de aplicar filtros */}
-      <button className= {styles.button} onClick={() => setFilteredbooks(filteredbooks)}>Aplicar Filtros</button>
-      
-      {/* Lista de Bookos filtrados */}
-      <div className={styles.container} >
-        {filteredbooks.map(Book => (
+      <button className={styles.button} onClick={applyFilters}>
+        Aplicar Filtros
+      </button>
+
+      {/* Lista de libros filtrados */}
+      <div className={styles.container}>
+        {filteredBooks.map((book, index) => (
           <CardBook
-            key={Book.id}
-            id={Book.id}
-            title={Book.title}
-            authors={Book.authors}
-            price={Book.price}
-            Tags={Book.Tags}
-            image={Book.image}
-            ratingCount={Book.ratingCount}
+            key={index}
+            id_book={book.id_book}
+            title={book.title}
+            Authors={book.Authors}
+            price={book.price}
+            Tags={book.Tags}
+            image={book.image}
+            rating_ave={book.rating_ave}
           />
         ))}
       </div>
-
     </div>
   );
-}
+};
 
 export default Filtros;
